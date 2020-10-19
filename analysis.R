264c3de4fc802ce9d9f98533e4fb45746952ba78
Ellie Kewin <efk507@york.ac.uk>
2020-10-19 15:44:19 +0100

    create data set and summarised

---
+++ analysis.R
@@ -0,0 +1,6 @@
+# Make data set
+
+df <- data.frame(x = rnorm(50,16,4), group = rep(c("group1", "group2"), each = 25))
+library(tidyverse)
+df %>% group_by(group) %>%
+summarise(mean = mean(x), n = length(n))
